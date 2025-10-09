758f9cb862b971e2bbaa2a10bd3ecebbe1ad8819
Manon <manon.gautier.web@gmail.com>
2025-10-09 12:36:13 +0200

    Initial commit

---
+++ src/main/java/org/example/altn72_projet_sara_theo_manon/Altn72ProjetSaraTheoManonApplication.java
@@ -0,0 +1,13 @@
+package org.example.altn72_projet_sara_theo_manon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Altn72ProjetSaraTheoManonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Altn72ProjetSaraTheoManonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/altn72_projet_sara_theo_manon/Altn72ProjetSaraTheoManonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.altn72_projet_sara_theo_manon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Altn72ProjetSaraTheoManonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
